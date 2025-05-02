c316f47ce9f56b13b3d7dc04a3607cc668efa2b7
Khaled11365 <khaledasser15@gmail.com>
2025-05-02 22:42:34 +0300

    Update test_bench_basic_computer.v

--- test_bench_basic_computer.v
+++ test_bench_basic_computer.v
@@ -1,22 +1,26 @@
-//vivado_TESTBENCH
-
 `timescale 1ns/1ps
-module BasicComputer_tb();
 
-reg clk;
-wire [15:0] IR;
-wire [15:0] TR;
-wire [15:0] DR;
-wire [15:0] AC;
-wire [11:0] PC;
-wire [11:0] AR;
-wire [3:0] timeCount;
+module tb_BasicComputer;
+    reg clk = 0, reset;
+    reg [7:0] INPR;
+    wire [7:0] OUTR, AC, IR;
+    wire [3:0] PC, AR;
+    wire       halt;
 
-// instantiate device under test
-BasicComputer dut(IR,TR,DR,AC,PC,AR,clk,timeCount);
+    BasicComputer_Top DUT(
+        .clk(clk), .reset(reset), .INPR(INPR),
+        .OUTR(OUTR), .AC(AC), .IR(IR), .PC(PC), .AR(AR), .halt(halt)
+    );
 
-always begin
-	clk = 0; #5; clk = 1; #5;
-end
+    always #2.5 clk = ~clk;
 
+    initial begin
+        $display("=== Simulation Start ===");
+        $readmemh("mem_data2.txt", DUT.mem_inst.mem);
+        INPR = 8'h0F;
+        reset = 1; #10; reset = 0;
+        #1000;
+        $display("=== Simulation End ===");
+        $stop;
+    end
 endmodule
